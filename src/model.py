f07ff10aea42b1a16d1e186bbac9bf592e1d5619
Vic Liu <144785904+VicLiu12@users.noreply.github.com>
2026-07-15 21:22:38 +0800

    program update
    update model.py

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ def load_config(config_path = "config.yaml"):
     with open(config_path, "r") as file:
         return yaml.safe_load(file)
     
-class DetectModel(nn.Model):
+class DetectModel(nn.Module):
     def __init__(self, config):
         super(DetectModel, self).__init__()
         
@@ -15,18 +15,26 @@ class DetectModel(nn.Model):
         num_classes = config['system']['num_classes']
         pretrained = config['model']['pretrained']
         
+        self.feature_map = None
+        
         if model_name == "resnet50" :
             weights = models.ResNet50_Weights.DEFAULT if pretrained else None
             self.backbone = models.resnet50(weights = weights)
             
             in_features = self.backbone.fc.in_features
             self.backbone.fc = nn.Linear(in_features, num_classes)
+            
+            self.backbone.layer4.register_forward_hook(self.hook_fn)
         
         else :
             raise ValueError("Model unsupported")
+    
+    def hook_fn(self, module, input, output):
+        self.feature_map = output
         
     def forward(self, x):
-        return self.backbone(x)
+        classification_result = self.backbone(x)
+        return classification_result, self.feature_map
     
 if __name__ == "__main__":
     config = load_config("../config.yaml")
@@ -36,9 +44,10 @@ if __name__ == "__main__":
     
     test_input = torch.randn(config['train']['batch_size'], 3, 224, 224)
     
-    output = model(test_input)
+    output_class, output_feature = model(test_input)
     print(f"輸入維度 : {test_input.shape}")
-    print(f"輸出維度 : {output.shape} (Batch Size, 類別數)")
+    print(f"輸出維度 : {output_class.shape} (Batch Size, 類別數)")
+    print(f"特徵圖輸出維度 : {output_feature.shape} (Batch Size, 特徵通道, 寬, 高)")
 
 
 
