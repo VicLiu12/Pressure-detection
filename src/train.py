686d8674cfea67d96a89b5a70dfe5bc5e8f84b2f
Vic Liu <144785904+VicLiu12@users.noreply.github.com>
2026-07-21 13:20:01 +0800

    Update train.py
    add class SAM

--- src/train.py
+++ src/train.py
@@ -16,6 +16,50 @@ from loss import FocalLoss
 from visualize import Grad_CAM
 
 
+class SAM(torch.optim.Optimizer):
+    def __init__(self, parmas, base_optimizer, rho = 0.05, **kwarge):
+        assert rho >= 0.0, f"Invalid rho, should be non-negative: {rho}"
+        defaults = dict(rho = rho, **kwarge)
+        super(SAM, self).__init__(parmas, defaults)
+        self.base_optimizer = base_optimizer(self.param_groups, **kwarge)
+        self.param_groups = self.base_optimizer.param_groups
+        
+    @torch.no_grad()
+    def first_step(self, zero_grad = False):
+        grad_norm = self._grad_norm()
+        for group in self.param_groups:
+            scale = group["rho"]/ (grad_norm + 1e-12)
+            for p in group["params"]:
+                if p.grad is None : continue
+                e_w = p.grad * scale.to(p)
+                p.add_(e_w)
+                self.state[p]["e_w"] = e_w 
+                
+        if self.zero_grad : self.zero_grad()
+        
+    @torch.no_grad()
+    def second_step(self, zero_grad = False):
+        for group in self.param_groups:
+            for p in group["params"]:
+                if p.grad is None : continue
+                p.sub_(self.state[p]["e_w"])
+                
+        self.base_optimizer.step()
+        
+        if zero_grad : self.zero_grad()
+        
+    def _grad_norm(self):
+        norm = torch.norm(
+            torch.stack([
+                p.grad.norm(p = 2)
+                for group in self.parma_groups for p in group["params"]
+                if p.grad is not None
+            ]),
+            p = 2
+        ) 
+        return norm
+
+
 def save_image(model,image_tansor, epoch, save_dir):
     cam_main = Grad_CAM(model)
     
