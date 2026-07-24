e1f30d51e1df8e3b977bf6179fbfe77ec9a60a51
Vic Liu <144785904+VicLiu12@users.noreply.github.com>
2026-07-24 18:11:36 +0800

    Upadte loss.py
    change loss

--- src/loss.py
+++ src/loss.py
@@ -2,21 +2,31 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class FocalLoss(nn.Module):
+class Loss_function(nn.Module):
     
-    def __init__(self, alpha = 1.0, gamma = 2.0):
-        super(FocalLoss, self).__init__()
+    def __init__(self, alpha = 1.0, gamma = 2.0, l2_reg = 0.1):
+        super(Loss_function, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
+        self.l2_reg = l2_reg
         
+        #[Invalid, SDTI, Stage_I, Stage_II, Stage_III, Stage_IV, Unstageable]
+        prior_matrix = [
+            [1.0, 5.0, 2.0, 3.0, 4.0, 5.0, 5.0], #Invalid
+            [6.0, 1.0, 4.0, 3.0, 3.0, 4.0, 3.0], #SDTI
+            [2.0, 3.0, 1.0, 1.5, 3.0, 5.0, 4.0], #Stage_I
+            [4.0, 2.0, 1.5, 1.0, 1.5, 3.0, 3.0], #Stage_II
+            [6.0, 3.0, 4.0, 1.5, 1.0, 1.5, 1.5], #Stage_III
+            [8.0, 4.0, 6.0, 3.0, 1.5, 1.0, 1.5], #Stage_IV
+            [8.0, 3.0, 6.0, 4.0, 1.5, 1.5, 1.0]  #Unstageble
+        ]
+        
+        self.register_buffer("prior_matrix", torch.tensor(prior_matrix, dtype = torch.float32))
+        
+        self.dynamic_matrix = nn.Parameter(torch.tensor(prior_matrix, dtype = torch.float32))
+        
+    
     def forward(self, inputs, targets):
-        ce_loss = F.cross_entropy(inputs, targets, reduction = 'none')
-        
-        pt = torch.exp(-ce_loss)
-        
-        focal_loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
-        
-        return focal_loss.mean()
-
+        ce_loss = 
 
 
