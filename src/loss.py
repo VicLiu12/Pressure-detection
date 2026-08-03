263730e4c226806d22486fd9e694fe433e501c2e
Vic Liu <144785904+VicLiu12@users.noreply.github.com>
2026-08-03 19:08:49 +0800

    Update loss.py
    add Ordinal SupCon Loss

--- src/loss.py
+++ src/loss.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Loss_function(nn.Module):
+class Cost_Focal_Loss(nn.Module):
     
     def __init__(self, alpha = 1.0, gamma = 2.0, l2_reg = 0.1):
-        super(Loss_function, self).__init__()
+        super(Cost_Focal_Loss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
         self.l2_reg = l2_reg
@@ -50,5 +50,45 @@ class Loss_function(nn.Module):
         reg_loss = self.l2_reg * torch.norm(self.dynamic_matrix - self.prior_matrix)
         
         return final_loss + reg_loss
+    
+class OrdinalSupConLoss(nn.Module):
+    def __init__(self, temperature = 0.07):
+        super(OrdinalSupConLoss, self).__init__()
+        self.temperature = temperature
+        
+        #不同分類的相互推開的力度
+        #[Ivalid, SDTI, StageI, StageII, StageIII, StageIV, Unstageable]
+        ordinal_levels = [0.0, 3.5, 1.0, 2.0, 3.0, 4.0, 3.5]
+        self.register_buffer("ordinal_levels", torch.tensor(ordinal_levels, dtype = torch.float32))
+        
+    def forward(self, feature, labels):
+        #feature : 來自 projection_head 且已 L2 Normalize 的特徵
+        device = feature.device
+        batch_size = feature.shape[0]
+        
+        #計算Batch中樣本間的相似度距離
+        sim_matrix = torch.matmul(feature, feature.T) / self.temperature
+        
+        sim_matrix_max, _ = torch.max(sim_matrix, dim = 1, keepdim = True)
+        logits = sim_matrix - sim_matrix_max.detach()
+        
+        #同類別的樣本位置設為 1
+        labels = labels.view(-1, 1)
+        mask = torch.eq(labels, labels.T).float().to(device)
+        
+        logits_mask = torch.scatter(
+            torch.ones_like(mask),
+            1,
+            torch.arange(batch_size).view(-1, 1).to(device),
+            0
+        )
+        mask = mask * logits_mask
+        
+        #計畫Batch內的階層距離，距離越遠則給予越大的排斥利
+        sample_levels = self.ordinal_levels[labels.squeeze()]
+        level_diff = torch.abs(sample_levels.view(-1, 1) - sample_levels.view(1, -1))
+        distance_weight = torch.ones_like(level_diff) + (1.0 - mask) * level_diff
+        
+        
 
 
